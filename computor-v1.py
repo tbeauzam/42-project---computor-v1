a7022a801bed13237ab75a992486f8f7bbdd9d72
tbeauzam <tbeauzam@student.42.fr>
2019-04-03 19:54:30 +0200

    Parser almost done

--- computor-v1.py
+++ computor-v1.py
@@ -1,49 +1,147 @@
-import math
 import sys
 import re
 
-term_matcher = r"(\d*(?:\.\d+)?(?: ?\* ?[a-zA-Z]|[a-zA-Z])(?: ?\^ ?\d*)?|\d+(?:\.\d+)?)"
+###
+# OBJECT
+###
 
 class Polynom:
+
+    #
+    # Constants
+    #
+
+    zero_expr = re.compile(r"^(?:([-+]?\d+(?:\.\d+)?)|([-+]?\d*(?:\.\d+)?) ?\*? ?(?:x\^0|1))$")
+    one_expr = re.compile(r"^([-+]?\d*(?:\.\d+)?) ?\*? ?(?:x\^1|x)$")
+    two_expr = re.compile(r"^([-+]?\d*(?:\.\d+)?) ?\*? ?x\^2$")
+    any_other_expr = re.compile(r"^([-+]?\d*(?:\.\d+)?) ?\*? ?x\^(\d+)$")
+
+    valid_charset = "0123456789.*-+=x^"
 
     #
     # Builtins
     #
 
     def __init__(self, expression):
-        """init
-
-        Args:
-            expression (string): expects a polynomial equation. The string will be parsed and the program will stop
-            if any error occurs
-        """
-        self.input = expression
-        print(self.input)
-
-        self.reduced = self.parse_input(self.input)
+        self.to_parse = expression
+        self.reduced = self.parse_input(self.to_parse)
 
 
     def __repr__(self):
-        """Representation"""
         rep = "- Polynomial equation info -\n"
-        rep += "input: " + self.input
+        rep += "input: " + self.to_parse
         return rep
 
     #
     # Public
     #
 
-    def parse_input(self, input):
-        reduced = input
+    def parse_input(self, to_parse):
+
+        print("Received input:")
+        print(to_parse)
+
+        to_parse = to_parse.lower()
+        print("\nTo lower case:")
+        print(to_parse)
+
+        to_parse = re.sub(r"\s", "", to_parse)
+        print("\nRemoved spaces:")
+        print(to_parse)
+
+        if to_parse.count("=") != 1 or not self.check_characters(to_parse):
+            display_error("the provided input is not a polynomial equation, or contains unexpected characters")
+            display_usage()
+            exit(0) 
+
+        while re.search(r"(--|\+\+|-\+|\+-)", to_parse):
+            to_parse = re.sub(r"(\+\+|--)", "+", to_parse)
+            to_parse = re.sub(r"(\+-|-\+)", "-", to_parse)
+        print("\nChecked signs:")
+        print(to_parse)
+
+        to_parse = re.sub(r"([+-])", r" \1", to_parse)
+        to_parse = re.sub(r"\s{2,}", " ", to_parse)
+        to_parse = re.sub(r"^ -", "-", to_parse)
+        to_parse = re.sub(r"= -", "=-", to_parse)
+        print("\nRemade spacing:")
+        print(to_parse)
+
+        lhs = to_parse.split("=")[0]
+        rhs = to_parse.split("=")[1]
+        print("\nLeft part: " + lhs)
+        print("Right part: " + rhs)
+
+        lhs = lhs.split(" ")
+        rhs = rhs.split(" ")
+        i = 0
+        for member in lhs:
+            if member[0] not in "-+":
+                lhs[i] = "+" + member
+            i += 1
+
+        for member in rhs:
+            if member[0] == "-":
+                member = "+" + member[1:]
+            elif member[0] == "+":
+                member = "-" + member[1:]
+            else:
+                member = "-" + member
+            lhs.append(member)
+        print("\nTransfer right terms to the left:")
+        print(lhs)
+
+        reduced = self.parse_terms(lhs)
         return reduced
 
 
+    def parse_terms(self, terms):
+        reduced_degrees = dict()
+        i = 0
+        for term in terms:
+            result = re.search(self.zero_expr, term)
+            if result:
+                tmp = float(result.group(1)) if result.group(1) else float(result.group(2))
+                if 0 in reduced_degrees:
+                    reduced_degrees += tmp
+                else:
+                    reduced_degrees = tmp
+                print("Found:")
+                print(tmp)
+            
+            print("Current value of reduced_degrees:")
+            print(reduced_degrees)
+
+
+
+    def check_characters(self, to_parse):
+        for c in to_parse:
+            if c not in self.valid_charset:
+                return False
+        return True
+
+
+
+###
+# OTHER FUNCTIONS
+###
+
 def display_usage():
-    print("Usage:\n --> ./computor \"polynomial equation\"")
+    message = "\nUsage:\n --> python3 computor-v1.py \"polynomial equation\""
+    message += "\n\nExmaple of polynomial equation : 1 * X^0 + 2 * X^1 = - 1 * X^0 + 4 * X^1"
+    message += "\n\nNotes:\n"
+    message += "- polynomial degree of reduced form must be 0, 1 or 2\n"
+    message += "- no negative or non-integer exponents allowed (valid range: 0 to 99)\n"
+    message += "- valid charset: |" + Polynom.valid_charset +"|\n- x can be written x or X, but every occurence will be lower-cased\n"
+    print(message)
 
 def display_error(message):
-    print("Error: " + message, file=sys.stderr)
+    print("\nError: " + message, file=sys.stderr)
 
+
+###
+# MAIN
+###
 
 def main():
 
@@ -54,7 +152,7 @@ def main():
 
     equation = Polynom(sys.argv[1])
 
-
+# Execute main if this script is directly executed
 if __name__ == "__main__":
     main()
 
---
+++ valid_randexpr_gen.py
@@ -0,0 +1,102 @@
+import random
+import sys
+import re
+
+degree_zero = [ "%s * x^0",
+                "%s * 1",
+                "%sx^0",
+                "%s",
+                "x^0",
+                "1" ]
+
+degree_one = [  "%s * x^1",
+                "%s * x",
+                "%sx",
+                "x^1",
+                "x" ]
+
+degree_two = [  "%s * x^2",
+                "%sx^2",
+                "x^2",
+
+MIN_RANGE = 0
+MAX_RANGE = 100
+
+def isInt(nb):
+    try:
+        int(nb)
+    except ValueError:
+        return 0
+    return 1
+
+
+def select_zero(empty_treshold, minus_treshold):
+    rand = random.randint(0, 99)
+    to_insert = ""
+    if rand < empty_treshold:
+        return ""
+    elif rand < minus_treshold:
+        to_insert += "-"
+    random.shuffle(degree_zero)
+    to_insert += degree_zero[0]
+    if to_insert.find("%s") != -1:
+        to_insert = to_insert % (random.randint(MIN_RANGE, MAX_RANGE))
+    return to_insert + " "
+
+
+def select_one(empty_treshold, minus_treshold):
+    rand = random.randint(0, 99)
+    to_insert = ""
+    if rand < empty_treshold:
+        return ""
+    to_insert += "- " if random.randint(0, 99) < minus_treshold else "+ "
+    random.shuffle(degree_one)
+    to_insert += degree_one[0]
+    if to_insert.find("%s") != -1:
+        to_insert = to_insert % (random.randint(MIN_RANGE, MAX_RANGE))
+    return to_insert + " "
+
+
+def select_two(empty_treshold, minus_treshold):
+    rand = random.randint(0, 99)
+    to_insert = ""
+    if rand < empty_treshold:
+        return ""
+    to_insert += "- " if random.randint(0, 99) < minus_treshold else "+ "
+    random.shuffle(degree_two)
+    to_insert += degree_two[0]
+    if to_insert.find("%s") != -1:
+        to_insert = to_insert % (random.randint(MIN_RANGE, MAX_RANGE))
+    return to_insert + " "
+
+def generate_terms():
+    rand = random.randint(0, 99)
+    if rand < 30:
+        return "0"
+    return select_zero(20, 40) + select_one(20, 40) + select_two(20, 40)
+
+def main():
+    print("How many lines do you want?")
+    nb = ""
+    while not isInt(nb):
+        nb = input("Number here ==> ")
+    nb = int(nb)
+    nb = 0 if nb < 0 else nb
+    nb = 100 if nb > 100 else nb
+
+    print(str(nb) + "? Here you go:")
+    for i in range(nb):
+        lhs = select_zero(10, 30) + select_one(10, 25) + select_two(10, 25)
+        rhs = generate_terms()
+        lhs = re.sub(r"^\+ ", "", lhs)
+        lhs = re.sub(r"^- ", "-", lhs)
+        rhs = re.sub(r"^\+ ", "", rhs)
+        rhs = re.sub(r"^- ", "-", rhs)
+        lhs = "0 " if lhs == "" else lhs
+        rhs = "0" if rhs == "" else rhs
+        expression = lhs + "= " + rhs
+        print(expression)
+
+
+if __name__ == "__main__":
+    main()
